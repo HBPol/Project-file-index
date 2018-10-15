f927c03ab1035690ee124010a1f6d9faf144bc31
hbp_01 <hector.bolsa@outlook.com>
2018-10-15 02:45:38 +0100

    Details view

--- webapp/docindex/views.py
+++ webapp/docindex/views.py
@@ -1,15 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, Http404
 from .models import StudyPlan
+from django.template import loader
 # Create your views here.
 def index(request):
-    #return HttpResponse("This is the list of study plans.")
     study_plan_list = StudyPlan.objects.order_by('id')
-    output = ', '.join([sp.title for sp in study_plan_list])
-    return HttpResponse(output)
+    context = {
+        'study_plan_list': study_plan_list,
+    }
+    return render(request, 'docindex/index.html', context)
     
 def detail(request, study_plan_id):
-    return HttpResponse("You are looking at study plan number %s." %study_plan_id)
+    study_plan = get_object_or_404(StudyPlan, pk=study_plan_id)
+    return render(request, 'docindex/detail.html', {'study_plan': study_plan})
 
 def addnew(request):
     return HttpResponse("You are going to add a new study plan.")
