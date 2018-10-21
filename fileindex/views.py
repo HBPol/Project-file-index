10bb61b49cd9ff53503fd365a62a8a562aa4dc38
hbp_01 <hector.bolsa@outlook.com>
2018-10-21 23:41:46 +0100

    Starting the Home page & views for the projects model.

--- fileindex/views.py
+++ fileindex/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from fileindex.models import Project, Location, StudyPlan, Report, RelatedFile, LabBook
+
+# Generate counts of some of the main objects
+def index(request):
+    num_projects = Project.objec.all().count()
+    num_study_plans = StudyPlan.object.all().count()
+    
+    
+    context = {
+        'num_projects': num_projects,
+        'num_study_plans': num_study_plans,
+    }
+    
+    return render(request, 'index.html', context=context)
